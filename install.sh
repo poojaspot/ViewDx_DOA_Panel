96b3998993a19800e52a78fb8290e4bfd8d7671b
Pooja Mehta <120557532+poojaspot@users.noreply.github.com>
2026-01-29 16:52:51 +0530

    Update GitHub repository URL in install script

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ set -e
 
 TOKEN="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"
 
-pip install --no-deps git+https://$TOKEN@github.com/SpotSensein/meril_doa/commits/viewdx_doa
+pip install --no-deps \
+  git+https://$TOKEN@github.com/poojaspot/ViewDx_DOA_Panel.git@viewdx_doa
